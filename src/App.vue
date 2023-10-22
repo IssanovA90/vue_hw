99ba37b2500a45bdb8a9ddd1bcce61f66557b59f
IssanovA90 <aslan.isanov.90@mail.ru>
2023-10-22 20:41:24 +0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,49 @@
+<template>
+  <div class="container">
+    <div class="colors" v-for="(color, index) in colors" :key="index">
+      <div class="block" :style="{ backgroundColor: color.hex }"></div>
+      <h3>{{ color.name }}</h3>
+      <p>{{ color.hex }}</p>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
-  data: () => {
+  data() {
     return {
-      
+      colors: [
+        { name: 'Red', hex: '#FF0000' },
+        { name: 'Green', hex: '#00FF00' },
+        { name: 'Blue', hex: '#0000FF' },
+        { name: 'Yellow', hex: '#FFFF00' },
+        { name: 'Cyan', hex: '#00FFFF' },
+        { name: 'Magenta', hex: '#FF00FF' },
+        { name: 'Black', hex: '#000000' },
+      ]
     };
-  },
-  computed: {},
-  methods: {},
+  }
 };
 </script>
 
-<template>
-
-</template>
-
 <style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.colors {
+  width: 200px;
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.block {
+  height: 100px;
+  margin-bottom: 10px;
+}
 </style>
