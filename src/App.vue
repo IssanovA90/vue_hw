dd1a76f4b555ebe80d2e448717f22b71d3209577
IssanovA90 <aslan.isanov.90@mail.ru>
2023-10-22 20:33:25 +0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <script>
 export default {
-  data: () => {
+  data() {
     return {
-      
+      name: '',
+      age: null
     };
   },
-  computed: {},
-  methods: {},
+  methods: {
+    submitForm() {
+      if (this.age < 0) {
+        alert(this.name +  ' Не может быть меньще 0');
+      } else if (this.age < 18) {
+        alert(this.name +  ' Вы еще не совершеннолетний');
+      } else if (this.age > 120) {
+        alert(this.name +  ' Вам больще 120 лет!');
+      } else {
+        alert('Привет, ' + this.name + ' твой возраст: ' +  this.age  );
+      }
+    }
+  },
 };
 </script>
 
 <template>
-
+  <div class="container">
+    <form @submit="submitForm">
+      <p>Имя:</p>
+      <input type="text" id="name" v-model="name" required>
+      <br>
+      <p>Возраст:</p>
+      <input type="number" id="age" v-model.number="age" required>
+      <br>
+      <button >Отправить</button>
+    </form>
+  </div>
 </template>
-
 <style scoped>
+.container{
+  padding: 40px;
+}
+
 </style>
