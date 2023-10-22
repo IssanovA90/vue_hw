edbeccf3b04a92faa116fc61f51bebbdf1795635
IssanovA90 <aslan.isanov.90@mail.ru>
2023-10-22 21:09:56 +0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
+<template>
+  <div class="container">
+    <textarea v-model="inputText" @input="format" placeholder="Введите текст..."></textarea>
+    <div>
+      <div class="italik" v-html="textItalik"></div>
+      <div class="bold" v-html="textBold"></div>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
-  data: () => {
+  data() {
     return {
-      
+      inputText: '',
+      textItalik: 'Италик',
+      textBold: 'Жирный',
     };
   },
-  computed: {},
-  methods: {},
+  methods: {
+    format() {
+      const boldText = `<b>${this.inputText}</b> `;
+      const italicText = `<i>${this.inputText}</i>`;
+      this.textItalik = italicText;
+      this.textBold = boldText;
+    },
+  },
 };
 </script>
 
-<template>
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
 
-</template>
+textarea {
+  width: 300px;
+  height: 100px;
+  padding: 10px;
+  margin-bottom: 20px;
+}
 
-<style scoped>
+.italik {
+  background-color: aqua;
+  padding: 10px;
+}
+.bold {
+  margin-top: 20px;
+  background-color: rgb(0, 255, 76);
+  padding: 10px;
+}
 </style>
